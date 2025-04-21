78dc7a81ac55d2eb732699be48f2bb3bd3ec5efa
Kai Wui Ye <kye5595@gmail.com>
2025-04-21 13:36:44 -0400

    html, background, header/footer, main

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName = "My name is Kai Wui Ye.";
+
+let greeting = "Welcome to my page!";
+
+let instructions = "Feel free to explore, connect, and reach out!";
+
+console.log(greeting, "\n", myName, "\n", instructions);
